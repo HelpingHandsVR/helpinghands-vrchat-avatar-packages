5e89bd018d982fd69dc60336f6ed1ea85b84cfd0
Devon R <sansgorialis@gmail.com>
2026-06-28 22:55:56 +0100

    Some pose encapsulation

--- Assets/HelpingHands/SignLanguageTools/Scripts/Editor/ExcludedFromPackage/MecanimAvatarDebug.cs
+++ Assets/HelpingHands/SignLanguageTools/Scripts/Editor/ExcludedFromPackage/MecanimAvatarDebug.cs
@@ -2,6 +2,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
@@ -9,10 +10,17 @@ namespace HelpingHandsVR.SignLanguageTools.Debug {
 
 public class MecanimAvatarDebug
 {
-    [MenuItem("CONTEXT/SkinnedMeshRenderer/HelpingHandsVR/Debug/PrintMecanimInfo")]
+    [MenuItem("CONTEXT/Animator/HelpingHandsVR/Debug/PrintMecanimInfo")]
     static void Debug_PrintInfo(MenuCommand command)
     {
-        UnityEngine.Debug.Log("placeholder");
+        Animator animator = (Animator)command.context;
+
+        var bonesAndAxes = Enumerable.Range(0, HumanTrait.BoneCount)
+            .Select(
+                i => (
+                    animator.GetBoneTransform((HumanBodyBones)i)
+                )
+            );
 
     }
 }
